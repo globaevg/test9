975a0c542f1c009b8acddc8f47d2314ff3139277
globaevg@gmail.com <globaevg@gmail.com>
2022-01-31 16:31:56 +0000

    Initial commit from Promethium

---
+++ target/run/my_new_project/models/example/my_second_dbt_model.sql
@@ -0,0 +1,8 @@
+
+  create view "dbt_test"."dbt_schema"."my_second_dbt_model__dbt_tmp" as (
+    -- Use the `ref` function to select from other models
+
+select *
+from "dbt_test"."dbt_schema"."my_first_dbt_model"
+where id = 1
+  );
